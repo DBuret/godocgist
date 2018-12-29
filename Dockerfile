2f93d788972651c427b6ae27307cf08d99198dda
DBuret <david.buret@gmail.com>
2018-12-29 22:45:53 +0100

    corrected build.sh and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM scratch
 LABEL maintainer=David.buret@gmail.com 
 EXPOSE 80
-COPY staticwebserver  index.html images  js  style gist /
-CMD ["staticwebserver"]
+COPY godocgist  index.html images  js  style gists /
+CMD ["godocgist"]
--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+set -x
+
 # use netgo so we do not need any shared lib
 GGO_ENABLED=0 GOOS=linux go build -a -tags netgo -ldflags "-w" .
 # save space
-strip staticwebserver
+strip godocgist
 
-chmod 755 staticwebserver
+chmod 755 godocgist
 
-sudo docker build -t dburet/docgist .
+sudo docker build -t registry.buret.cc/dburet/docgist .
 
 sudo docker images
