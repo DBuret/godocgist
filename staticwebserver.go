5c7cf7f9deafa3bae959fa31338df6bb0a494733
DBuret <david.buret@gmail.com>
2018-12-30 09:48:34 +0100

    debug

--- staticwebserver.go
+++ staticwebserver.go
@@ -9,7 +9,7 @@ func main() {
 
 	var basepath = "/docgist/"
 
-	http.Handle(basepath, http.StripPrefix(basepath,(http.FileServer(http.Dir("/"))))
+	http.Handle(basepath, http.StripPrefix(basepath,(http.FileServer(http.Dir("/") ))))
 
 	log.Fatal(http.ListenAndServe(":80", nil))
 }
